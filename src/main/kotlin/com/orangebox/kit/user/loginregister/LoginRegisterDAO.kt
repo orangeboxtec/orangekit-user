29a3301d24e15192a89ae90d44deebdc620543a5
getwlad <wladmir.rrodrigues@gmail.com>
2025-09-11 21:13:32 -0300

    [#86ab43dkw] Login Register - Add endpoint para trazer por varios usuarios

--- src/main/kotlin/com/orangebox/kit/user/loginregister/LoginRegisterDAO.kt
+++ src/main/kotlin/com/orangebox/kit/user/loginregister/LoginRegisterDAO.kt
@@ -273,4 +273,202 @@ class LoginRegisterDAO: AbstractDAO<LoginRegister>(LoginRegister::class.java) {
         return map
     }
 
+    fun loginsRegisterDaily(usersId: List<String>?, page: Int?): Map<String, Any> {
+        val (calendarInitial, calendarFinal) = getDailyPeriod(page)
+        val docs = aggregateLogins(
+            matchObj = buildMatchObj(calendarInitial.time, calendarFinal.time, usersId),
+            projectFields = dailyProjectFields(),
+            groupFields = dailyGroupFields(),
+            sortFields = BasicDBObject("_id", 1)
+        )
+        return mapOf(
+            "labels" to docs.map { (it["_id"].toString().toInt() - 3) },
+            "series" to docs.map { it["total"] },
+            "weekDay" to dayOfWeek[calendarInitial.get(Calendar.DAY_OF_WEEK)],
+            "date" to sdf.format(calendarInitial.time)
+        )
+    }
+
+    fun loginsRegisterWeekly(usersId: List<String>?, page: Int?): Map<String, Any> {
+        val (calendarInitial, calendarFinal) = getWeeklyPeriod(page)
+        val docs = aggregateLogins(
+            matchObj = buildMatchObj(calendarInitial.time, calendarFinal.time, usersId),
+            projectFields = weeklyProjectFields(),
+            groupFields = weeklyGroupFields(),
+            sortFields = BasicDBObject("_id", 1)
+        )
+        fillMissingDays(calendarInitial, docs)
+        docs.sortBy { it["dateToSort"].toString() }
+        return mapOf(
+            "labels" to docs.map { it["_id"] },
+            "series" to docs.map { it["total"] },
+            "initialDate" to sdf.format(calendarInitial.time),
+            "finaleDate" to sdf.format(calendarFinal.time)
+        )
+    }
+
+    fun loginsRegisterMonthly(usersId: List<String>?, page: Int?): Map<String, Any> {
+        val (calendarInitial, calendarFinal) = getMonthlyPeriod(page)
+        val docs = aggregateLogins(
+            matchObj = buildMatchObj(calendarInitial.time, calendarFinal.time, usersId),
+            projectFields = monthlyProjectFields(),
+            groupFields = monthlyGroupFields(),
+            sortFields = BasicDBObject("_id", 1)
+        )
+        fillMissingMonths(docs)
+        docs.sortBy { it["_id"].toString().toInt() }
+        docs.forEach { it["_id"] = monthStringShort[it["_id"].toString().toInt()] }
+        return mapOf(
+            "labels" to docs.map { it["_id"] },
+            "series" to docs.map { it["total"] },
+            "year" to "${calendarInitial.get(Calendar.YEAR)}"
+        )
+    }
+
+
+    private fun getDailyPeriod(page: Int?): Pair<Calendar, Calendar> {
+        val initial = Calendar.getInstance().apply { setToStartOfDay() }
+        val final = Calendar.getInstance().apply { setToEndOfDay() }
+        page?.let {
+            initial.add(Calendar.DAY_OF_WEEK, it - if (it > 0) 1 else 0)
+            final.add(Calendar.DAY_OF_WEEK, it - if (it > 0) 1 else 0)
+        }
+        return initial to final
+    }
+
+    private fun getWeeklyPeriod(page: Int?): Pair<Calendar, Calendar> {
+        val initial = Calendar.getInstance().apply { setToStartOfDay() }
+        val final = Calendar.getInstance().apply { setToEndOfDay() }
+        when {
+            page == null || page == 0 || page == 1 -> final.add(Calendar.DAY_OF_WEEK, 6)
+            page > 1 -> {
+                initial.add(Calendar.DAY_OF_WEEK, (page - 1) * 7)
+                final.add(Calendar.DAY_OF_WEEK, ((page - 1) * 7) + 6)
+            }
+            page == -1 -> {
+                initial.add(Calendar.DAY_OF_WEEK, -7)
+                final.add(Calendar.DAY_OF_WEEK, -1)
+            }
+            else -> {
+                initial.add(Calendar.DAY_OF_WEEK, (page + 1) * 7 - 7)
+                final.add(Calendar.DAY_OF_WEEK, (page + 1) * 7 - 1)
+            }
+        }
+        return initial to final
+    }
+
+    private fun getMonthlyPeriod(page: Int?): Pair<Calendar, Calendar> {
+        val initial = Calendar.getInstance().apply {
+            setToStartOfDay()
+            set(Calendar.DAY_OF_MONTH, 1)
+            set(Calendar.MONTH, 0)
+        }
+        val final = Calendar.getInstance().apply {
+            setToEndOfDay()
+            set(Calendar.DAY_OF_MONTH, 31)
+            set(Calendar.MONTH, 11)
+        }
+        page?.let {
+            initial.add(Calendar.YEAR, it - if (it > 1) 1 else 0)
+            final.add(Calendar.YEAR, it - if (it > 1) 1 else 0)
+        }
+        return initial to final
+    }
+
+    private fun buildMatchObj(start: Date, end: Date, usersId: List<String>?): BasicDBObject {
+        val obj = BasicDBObject("loginDate", BasicDBObject("\$gte", start).append("\$lte", end))
+        if (!usersId.isNullOrEmpty()) obj.append("userId", BasicDBObject("\$in", usersId))
+        return obj
+    }
+
+    private fun aggregateLogins(
+        matchObj: BasicDBObject,
+        projectFields: BasicDBObject,
+        groupFields: BasicDBObject,
+        sortFields: BasicDBObject
+    ): MutableList<Document> {
+        val collection = getDb().getCollection("loginRegister")
+        val pipeline = listOf(
+            BasicDBObject("\$match", matchObj),
+            BasicDBObject("\$project", projectFields),
+            BasicDBObject("\$group", groupFields),
+            BasicDBObject("\$sort", sortFields)
+        )
+        return collection.aggregate(pipeline).into(ArrayList())
+    }
+
+    private fun dailyProjectFields() = BasicDBObject("hour", BasicDBObject("\$dateToString", BasicDBObject("format", "%H").append("date", "\$loginDate")))
+        .append("count", BasicDBObject("\$toInt", "1"))
+        .append("userId", "\$userId")
+        .append("loginDate", "\$loginDate")
+        .append("weekDay", BasicDBObject("\$dayOfWeek", "\$loginDate"))
+        .append("dateF", BasicDBObject("\$dateToString", BasicDBObject("format", "%d/%m/%Y").append("date", "\$loginDate")))
+
+    private fun dailyGroupFields() = BasicDBObject("_id", "\$hour")
+        .append("total", BasicDBObject("\$sum", "\$count"))
+        .append("weekDay", BasicDBObject("\$first", "\$weekDay"))
+        .append("date", BasicDBObject("\$first", "\$dateF"))
+
+    private fun weeklyProjectFields() = BasicDBObject("day", BasicDBObject("\$dateToString", BasicDBObject("format", "%d").append("date", "\$loginDate")))
+        .append("count", BasicDBObject("\$toInt", "1"))
+        .append("userId", "\$userId")
+        .append("loginDate", "\$loginDate")
+        .append("dayString", BasicDBObject("\$dateToString", BasicDBObject("format", "%d").append("date", "\$loginDate")))
+        .append("dateToSort", BasicDBObject("\$dateToString", BasicDBObject("format", "%m%d").append("date", "\$loginDate")))
+
+    private fun weeklyGroupFields() = BasicDBObject("_id", "\$day")
+        .append("total", BasicDBObject("\$sum", "\$count"))
+        .append("dayString", BasicDBObject("\$first", "\$dayString"))
+        .append("dateToSort", BasicDBObject("\$first", "\$dateToSort"))
+
+    private fun monthlyProjectFields() = BasicDBObject("month", BasicDBObject("\$dateToString", BasicDBObject("format", "%m").append("date", "\$loginDate")))
+        .append("count", BasicDBObject("\$toInt", "1"))
+        .append("userId", "\$userId")
+        .append("loginDate", "\$loginDate")
+
+    private fun monthlyGroupFields() = BasicDBObject("_id", "\$month")
+        .append("total", BasicDBObject("\$sum", "\$count"))
+
+    private fun fillMissingDays(calendarInitial: Calendar, docs: MutableList<Document>) {
+        repeat(7) {
+            val day = calendarInitial.get(Calendar.DAY_OF_MONTH).toString()
+            if (docs.none { it["_id"] == day }) {
+                val monthString = (calendarInitial.get(Calendar.MONTH) + 1).toString().padStart(2, '0')
+                val dayString = calendarInitial.get(Calendar.DAY_OF_MONTH).toString().padStart(2, '0')
+                docs.add(Document().apply {
+                    this["_id"] = day
+                    this["total"] = 0
+                    this["dateToSort"] = monthString + dayString
+                })
+            }
+            calendarInitial.add(Calendar.DAY_OF_MONTH, 1)
+        }
+        calendarInitial.add(Calendar.DAY_OF_MONTH, -7)
+    }
+
+    private fun fillMissingMonths(docs: MutableList<Document>) {
+        for (month in 1..12) {
+            if (docs.none { it["_id"].toString().toInt() == month }) {
+                docs.add(Document().apply {
+                    this["_id"] = month.toString().padStart(2, '0')
+                    this["total"] = 0
+                })
+            }
+        }
+    }
+
+    private fun Calendar.setToStartOfDay() {
+        set(Calendar.HOUR_OF_DAY, 0)
+        set(Calendar.MINUTE, 0)
+        set(Calendar.SECOND, 0)
+        set(Calendar.MILLISECOND, 0)
+    }
+
+    private fun Calendar.setToEndOfDay() {
+        set(Calendar.HOUR_OF_DAY, 23)
+        set(Calendar.MINUTE, 59)
+        set(Calendar.SECOND, 59)
+        set(Calendar.MILLISECOND, 999)
+    }
+
 }
